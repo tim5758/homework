45286b957315ec12a1296f4d5196f939c0828056
徐培恩(Tim) <tim0972178753@gmail.com>
2023-01-13 19:29:38 +0800

    成功使用jdbc連上mysql，不需要去下載connector，只要在pom.xml加入dependency就好。

--- src/main/java/com/example/homework/dao/ProductDao.java
+++ src/main/java/com/example/homework/dao/ProductDao.java
@@ -11,6 +11,11 @@ public class ProductDao {
     public void getProducts() throws SQLException {
         String sql = "SELECT * FROM product";
 
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/homework", "root", "Password123");
         if(connection != null){
             System.out.println("success!");
